c09f0e98214fcd63d2e76232707653bdf8c15c73
CKHuang-public <chihkaihuang1@gmail.com>
2026-09-23 23:09:24 +0200

    test

--- 0-single-cluster/0-rennes/create-cluster.py
+++ 0-single-cluster/0-rennes/create-cluster.py
@@ -1,37 +1,35 @@
-import string
 import getpass
 from enoslib.api import generate_inventory, run_ansible
 import enoslib as en
 import time
-
-en.set_config(ansible_forks=100)
+import tomllib
 
 username = getpass.getuser()
-
-name = f"cluster-rennes-{username}"
-
-clusters = "paradoxe"
-
-site = "rennes"
+en.set_config(ansible_forks=100)
+with open("config.toml", "rb") as f:
+    config = tomllib.load(f)
 
 cp_nodes = []
-
 all_vm_nodes = []
 
-duration = "03:00:00"
+cluster = config["g5k"]["cluster"]
+site = config["g5k"]["site"]
+duration = config["g5k"]["duration"]
+number_servers = config["g5k"]["number_servers"]
+suffix = config["g5k"]["suffix"]
+
+job_name = f"{site}-{cluster}-{username}-{suffix}"
 
 prod_network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=site)
 
-name_job = name +"-"+clusters
-
 conf = (
-    en.G5kConf.from_settings(job_type=[], job_name=name_job, walltime=duration)
+    en.G5kConf.from_settings(job_type=[], job_name=job_name, walltime=duration)
     .add_network_conf(prod_network)
     .add_network(
         id="not_linked_to_any_machine", type="slash_22", roles=["my_subnet"], site=site
     )
     .add_machine(
-    roles=["role0"], cluster=clusters, nodes=1, primary_network=prod_network
+    roles=["role0"], cluster=cluster, nodes=number_servers, primary_network=prod_network
     ).finalize()
 )
 
@@ -42,31 +40,31 @@ roles = en.sync_info(roles, networks)
 subnet = networks["my_subnet"]
 
 cp = 1
-w1 = 1
+w1 = config["vm"]["worker"]["number"]
 
-for i in range(0,1):
+for i in range(config["deployment"]["number_k8s"]):
     start = i * (cp + w1)
     virt_conf = (
-        en.VMonG5kConf.from_settings(image=f"/home/{username}/images/debian13-k8s-large.qcow2")
+        en.VMonG5kConf.from_settings(image = config["vm"]["image"])
         .add_machine(
             roles=["cp"],
             number=cp,
             undercloud=roles["role0"],
-            flavour_desc={"core": 8, "mem": 16384},
+            flavour_desc={"core": config["vm"]["control_plane"]["cpu"], "mem": config["vm"]["control_plane"]["memory"]},
             macs=list(subnet[0].free_macs)[start:start+cp],
         )
         .add_machine(
             roles=["member"],
             number=w1,
             undercloud=roles["role0"],
-            flavour_desc={"core": 8, "mem": 16384},
+            flavour_desc={"core": config["vm"]["worker"]["cpu"], "mem": config["vm"]["worker"]["memory"]},
             macs=list(subnet[0].free_macs)[start+cp:start+cp+w1],
         ).finalize()
     )
 
     vmroles = en.start_virtualmachines(virt_conf,force_deploy=True)
 
-    tempname=name_job+str(i)
+    tempname = job_name + str(i)
 
     inventory_file = "kubefed_inventory_cluster"+ str(tempname) +".ini" 
 
@@ -79,9 +77,9 @@ for i in range(0,1):
     for vm in vmroles["member"]:
         all_vm_nodes.append(vm.address)
 
-    time.sleep(45)
+    time.sleep(config["deployment"]["wait_before_ansible"])
 
-    run_ansible(["k8s.yml"], inventory_path=inventory_file)
+    run_ansible([config["deployment"]["ansible_playbook"]],inventory_path=inventory_file)
 
 with open("cp_node_list", "a") as f:
     for ip in cp_nodes:
--- 0-single-cluster/0-rennes/delete-cluster.py
+++ 0-single-cluster/0-rennes/delete-cluster.py
@@ -1,37 +1,35 @@
-import string
 import getpass
 from enoslib.api import generate_inventory, run_ansible
 import enoslib as en
 import time
-
-en.set_config(ansible_forks=100)
+import tomllib
 
 username = getpass.getuser()
-
-name = f"cluster-rennes-{username}"
-
-clusters = "paradoxe"
-
-site = "rennes"
+en.set_config(ansible_forks=100)
+with open("config.toml", "rb") as f:
+    config = tomllib.load(f)
 
 cp_nodes = []
-
 all_vm_nodes = []
 
-duration = "03:00:00"
+cluster = config["g5k"]["cluster"]
+site = config["g5k"]["site"]
+duration = config["g5k"]["duration"]
+number_servers = config["g5k"]["number_servers"]
+suffix = config["g5k"]["suffix"]
+
+job_name = f"{site}-{cluster}-{username}-{suffix}"
 
 prod_network = en.G5kNetworkConf(type="prod", roles=["my_network"], site=site)
 
-name_job = name +"-"+clusters
-
 conf = (
-    en.G5kConf.from_settings(job_type=[], job_name=name_job, walltime=duration)
+    en.G5kConf.from_settings(job_type=[], job_name=job_name, walltime=duration)
     .add_network_conf(prod_network)
     .add_network(
         id="not_linked_to_any_machine", type="slash_22", roles=["my_subnet"], site=site
     )
     .add_machine(
-    roles=["role0"], cluster=clusters, nodes=1, primary_network=prod_network
+    roles=["role0"], cluster=cluster, nodes=number_servers, primary_network=prod_network
     ).finalize()
 )
 
